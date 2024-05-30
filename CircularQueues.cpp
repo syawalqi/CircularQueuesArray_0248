678e4a4842e15d7d0931065206cdf5a602ab4d92
syawalqi <galihmaulanasyawalqi@gmail.com>
2024-05-30 15:20:42 +0700

    membuat  if (FRONT_Position <= REAR_Position)

--- CircularQueues.cpp
+++ CircularQueues.cpp
@@ -70,6 +70,15 @@ class Queues {
             cout << "Queue is empty\n";
             return;
         }
+
+        cout << "\nElements in the queue are...\n";
+
+        //jika FRONT <= REAR, iterasi dari FRONT hingga REAR
+        if (FRONT_Position <= REAR_Position) {
+            while (FRONT_Position <= REAR_Position) {
+                cout << queue_array [FRONT_Position] << "\n";
+            }
+        }
     }
 
 };
