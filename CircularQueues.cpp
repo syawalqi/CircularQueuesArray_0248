bd091fdbf4bbbcc48c87581b5633701a435b5b91
syawalqi <galihmaulanasyawalqi@gmail.com>
2024-05-30 14:49:39 +0700

    membuat else if

--- CircularQueues.cpp
+++ CircularQueues.cpp
@@ -23,6 +23,13 @@ class Queues {
             FRONT = 0;
             REAR = 0;
         }
+        else {
+            if (
+                REAR == max - 1)
+                REAR = 0;
+            else
+            REAR = REAR + 1
+        }
     }
 
 };
